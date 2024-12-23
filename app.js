e4a8c4cdaa9ea42db77c0ee32d9d27f5bf2e1918
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-22 20:53:16 -0500

    CORS Delete

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   // Attach CORS headers
   // Required when using a detached backend (that runs on a different domain)
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
